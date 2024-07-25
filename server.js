d9028c4a4282b857dd709292043d215d48c4dc50
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-07-25 22:39:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL,
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true,
     }
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true,
     })
